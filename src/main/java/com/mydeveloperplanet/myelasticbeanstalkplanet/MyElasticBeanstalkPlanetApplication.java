afac9ae6a5df83f27fc480c1eb9ff6bedb6a4df6
mydeveloperplanet <gunter@mydeveloperplanet.com>
2020-09-27 10:22:05 +0200

    Initial commit

---
+++ src/main/java/com/mydeveloperplanet/myelasticbeanstalkplanet/MyElasticBeanstalkPlanetApplication.java
@@ -0,0 +1,13 @@
+package com.mydeveloperplanet.myelasticbeanstalkplanet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyElasticBeanstalkPlanetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyElasticBeanstalkPlanetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mydeveloperplanet/myelasticbeanstalkplanet/MyElasticBeanstalkPlanetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mydeveloperplanet.myelasticbeanstalkplanet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyElasticBeanstalkPlanetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
